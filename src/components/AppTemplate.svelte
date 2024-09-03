78ca3b76852ae8433dbb66395027f9e6a3b3a3b1
Ayveren <ayverend@gmail.com>
2024-09-03 16:38:38 +0200

    add logs

--- src/components/AppTemplate.svelte
+++ src/components/AppTemplate.svelte
@@ -22,11 +22,14 @@
     });
 
     const handleBeforeDestroy = () => {
+        console.log('stop casting');
         typeof AirServerAppStop === "function" && AirServerAppStop();
     };
 
     const handleKeyDown = (event) => {
+        console.log(event.key);
         if (event.key === 'Backspace') {
+            console.log('stop casting');
             typeof AirServerAppStop === "function" && AirServerAppStop();
         }
     };
